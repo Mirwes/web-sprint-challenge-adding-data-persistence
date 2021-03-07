0268c1bcf653b6a6764e14ca8ba4bf532e696a04
Mirwes <mirwes210@gmail.com>
2021-03-07 15:45:40 -0800

    codes added to model.js

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,26 @@
 // build your `Project` model here
+const dbConfig = require("../../data/dbConfig");
+
+function getProjects(){
+  return dbConfig('projects')
+    .select('*')
+}
+function getProjectById(id){
+  return dbConfig('projects')
+    .where({project_id: id})
+    .first();
+}
+function addProject(project){
+  return dbConfig('projects')
+    .insert(project)
+    .then(ids => {
+      return getProjectById(ids[0])
+    });
+}
+
+
+module.exports = {
+  getProjects,
+  getProjectById,
+  addProject
+}
