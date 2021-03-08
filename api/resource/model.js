c59ded038b2f2980f93f808ddae1a2ac429c94d4
Mirwes <mirwes210@gmail.com>
2021-03-07 16:05:09 -0800

    resource_name and description has been added

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,32 @@
 // build your `Resource` model here
+const dbConfig = require("../../data/dbConfig");
+
+function getResources(){
+  return dbConfig('resources')
+    .select('*')
+}
+function getResourceById(id){
+  return dbConfig('resources')
+    .select('resource_name')
+    .where({resource_id: id})
+}
+function getResourceByName(name){
+  return dbConfig('resources')
+    .where({resource_name: name})
+    .first()
+}
+function addResource(resource){
+  return dbConfig('resources')
+    .insert(resource)
+    .then(ids => {
+      return getResourceById(ids[0])
+    });
+}
+
+
+module.exports = {
+  getResources,
+  getResourceById,
+  getResourceByName,
+  addResource
+}
